1d150f48ae873eadfd29ce339406aafceb23843d
Bartek <bartek@pop-os.home>
2025-02-02 13:05:36 +0100

    Fix BudgetAssignService and ServiceSpec

--- src/test/scala/com/cleverhouse/spendless/utils/ServiceSpec.scala
+++ src/test/scala/com/cleverhouse/spendless/utils/ServiceSpec.scala
@@ -3,7 +3,8 @@ package com.cleverhouse.spendless.utils
 import cats.effect.IO
 import cats.effect.testing.specs2.CatsEffect
 import cats.effect.unsafe.IORuntime
-import pl.iterators.kebs.scalacheck.KebsScalacheckGenerators
+import com.cleverhouse.spendless.auth.domain.AuthContext
+import pl.iterators.kebs.scalacheck.{Generator, KebsScalacheckGenerators}
 import com.cleverhouse.spendless.utils.db.PostgresIOTransactor
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.matchers.should.Matchers
@@ -19,6 +20,8 @@ trait ServiceSpec extends AnyWordSpec with CatsEffect with KebsScalacheckGenerat
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
   implicit val ioRuntime: IORuntime = cats.effect.unsafe.implicits.global
 
+  lazy val authGenerator: Generator[AuthContext] = allGenerators[AuthContext].normal
+
   trait BaseScope {
 
     implicit class AsDBIO[T](t: T) {
@@ -32,6 +35,8 @@ trait ServiceSpec extends AnyWordSpec with CatsEffect with KebsScalacheckGenerat
     }
 
     val transactor: PostgresIOTransactor = new FakeTransactor()
+
+    val authUser: AuthContext  = authGenerator.generate
   }
 
 }
